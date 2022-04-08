de26d59f486923385231578731c823cd29ae048f
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-08 18:08:06 +0530

    adding Employee practise problem

---
+++ EmployeePayrollOperation/EmployeePayrollOperation/EmployeeDetails.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollOperation
+{
+    public class EmployeeDetails
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+
+        public char Gender { get; set; }
+        public double BasicPay { get; set; }
+        public double Deduction { get; set; }
+
+        public double TaxablePay { get; set; }
+
+        public double Tax { get; set; }
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public EmployeeDetails(int employeeid, string employeename, string phonenumber, string department, char gender,
+            double basicpay, double deduction, double taxablepay, double tax, string city, string country)
+        {
+            this.EmployeeID = employeeid;
+            this.EmployeeName = employeename;
+            this.PhoneNumber = phonenumber;
+            this.Address = Address;
+            this.Department = department;
+            this.Gender = gender;
+            this.BasicPay = basicpay;
+            this.Deduction = deduction;
+            this.TaxablePay = taxablepay;
+            this.Tax = tax;
+            this.City = city;
+            this.Country = country;
+        }
+
+    }
+}
---
+++ EmployeePayrollOperation/EmployeePayrollOperation/EmployeePayrollOperation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollOperation
+{
+    public class EmployeePayrollOperation
+    {
+        List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+
+        public void addEmployeePayroll(List<EmployeeDetails> listEmployeeDetails)
+        {
+            listEmployeeDetails.ForEach(employeedata =>
+            {
+                Console.WriteLine("Employee being added : " + employeedata.EmployeeName);
+                this.addEmployeePayroll(employeedata);
+                Console.WriteLine("Employee added :" + employeedata.EmployeeName);
+            }
+            );
+            Console.WriteLine(this.employeeDetails.ToString());
+        }
+        private void addEmployeePayRoll(EmployeeDetails emp)
+        {
+            employeeDetails.Add(emp);
+        }
+
+        public void addEmployeePayRollwiththread(List<EmployeeDetails> listemployeedetails)
+        {
+            listemployeedetails.ForEach(employeedata =>
+         {
+             Task thread = new Task(() =>
+             {
+                 Console.WriteLine("Employee being added : " + employeedata.EmployeeName);
+                 this.addEmployeePayroll(employeedata);
+                 Console.WriteLine("Employee added :" + employeedata.EmployeeName);
+
+             });
+             thread.Start();
+         });
+    }
+    }
+}
+
